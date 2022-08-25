96d93716c32712bcf57361ea84982ed953193bb1
KoreanCodingMachine <pon03078@naver.com>
2022-08-25 22:36:09 +0900

    무한스크롤 수정중

--- src/components/main/MainRecentContainer.jsx
+++ src/components/main/MainRecentContainer.jsx
@@ -6,14 +6,19 @@ import axios from 'axios';
 
 const MainRecentContainer = () => {
   const [randomData, setRandomData] = useState([]);
-  const [page, setPage] = useState(0);
+  const pages = useRef(0);
+
+  const addPages = () => {
+    pages.current++;
+  };
+
   const [lastIntersectingData, setLastIntersectingData] = useState(null);
   //fetching 함수
   const getRandomData = async () => {
     console.log('fetching함수 호출됨');
     try {
       const { data } = await axios.get(
-        `http://3.38.48.108/api/post?Category=NEW&size=10&page=${page}`
+        `http://3.38.48.108/api/post?Category=NEW&size=10&page=${pages.current}`
       );
       setRandomData(randomData.concat(data));
       console.log(randomData);
@@ -22,16 +27,12 @@ const MainRecentContainer = () => {
     }
   };
 
-  // useEffect(() => {
-  //   getRandomData();
-  // }, []);
-
   //observe 콜백 함수
   const onIntersect = (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         //뷰포트에 마지막 이미지가 들어오고, page값에 1을 더하여 새 fetch 요청을 보내게됨 (useEffect의 dependency배열에 page가 있음)
-        setPage((prev) => prev + 1);
+        pages.current++;
         // 현재 타겟을 unobserve한다.
         observer.unobserve(entry.target);
       }
@@ -39,16 +40,16 @@ const MainRecentContainer = () => {
   };
 
   useEffect(() => {
-    console.log('page?', page);
+    console.log('page?', pages.current);
     getRandomData();
-  }, [page]);
+  }, [pages.current]);
 
   useEffect(() => {
     //observer 인스턴스를 생성한 후 구독
     let observer;
     if (lastIntersectingData) {
       observer = new IntersectionObserver(onIntersect, { threshold: 0.5 });
-      //observer 생성 시 observe할 target 요소는 불러온 이미지의 마지막아이템(randomImageList 배열의 마지막 아이템)으로 지정
+      //observer 생성 시 observe할 target 요소는 불러온 이미지의 마지막아이템(배열의 마지막 아이템)으로 지정
       observer.observe(lastIntersectingData);
     }
     return () => observer && observer.disconnect();
