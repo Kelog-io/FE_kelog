4658bee778f0989798058248fb484d2ba42fde16
KoreanCodingMachine <pon03078@naver.com>
2022-08-25 20:22:58 +0900

    무한 스크롤 구현중..

--- src/components/main/MainRecentContainer.jsx
+++ src/components/main/MainRecentContainer.jsx
@@ -1,93 +1,88 @@
 import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import MainPostCard from './MainPostCard';
-import { useDispatch, useSelector } from 'react-redux';
-import { getRecentData } from '../../redux/modules/mainPostSlice';
 import 'react-intersection-observer';
 import axios from 'axios';
 
 const MainRecentContainer = ({ data }) => {
-  const dispatch = useDispatch();
-
-  // 저장할 데이터 리스트
   const [randomData, setRandomData] = useState([]);
-
-  // 페이지
-  const [page, setPage] = useState(1);
-
-  // 관찰대상(entry)(마지막 이미지)
+  const [page, setPage] = useState(0);
   const [lastIntersectingData, setLastIntersectingData] = useState(null);
-
   //fetching 함수
   const getRandomData = async () => {
-    console.log('fetching 함수 호출');
+    console.log('fetching함수 호출됨');
     try {
       const { data } = await axios.get(
-        `https://picsum.photos/v2/list?page=${page}&limit=7`
+        `http://3.38.48.108/api/post?Category=NEW&size=10&page=${page}`
       );
-      console.log(data);
       setRandomData(randomData.concat(data));
+      console.log(randomData);
     } catch (error) {
       console.log(error);
     }
   };
 
+  // useEffect(() => {
+  //   getRandomData();
+  // }, []);
+
   //observe 콜백 함수
   const onIntersect = (entries, observer) => {
     entries.forEach((entry) => {
-      // 교차로 전한 , 즉 타겟(관찰대상)가 교차 상태에 들어오면
       if (entry.isIntersecting) {
         //뷰포트에 마지막 이미지가 들어오고, page값에 1을 더하여 새 fetch 요청을 보내게됨 (useEffect의 dependency배열에 page가 있음)
         setPage((prev) => prev + 1);
-        // 현재 타겟(관찰대상)을 unobserve한다.
-        observer.unobserve(entry.taret);
+        // 현재 타겟을 unobserve한다.
+        observer.unobserve(entry.target);
       }
     });
   };
 
-  // 페이지가 바뀌면 새로운 데이터를 패칭한다.
   useEffect(() => {
-    console.log('page ?', page);
+    console.log('page?', page);
     getRandomData();
   }, [page]);
 
   useEffect(() => {
+    //observer 인스턴스를 생성한 후 구독
     let observer;
     if (lastIntersectingData) {
       observer = new IntersectionObserver(onIntersect, { threshold: 0.5 });
+      //observer 생성 시 observe할 target 요소는 불러온 이미지의 마지막아이템(randomImageList 배열의 마지막 아이템)으로 지정
       observer.observe(lastIntersectingData);
     }
     return () => observer && observer.disconnect();
   }, [lastIntersectingData]);
-
   // 최신(Recent) 페이지
   // 최신 페이지
 
-  useEffect(() => {
-    dispatch(getRecentData());
-  }, []);
+  // useEffect(() => {
+  //   dispatch(getRecentData());
+  // }, []);
 
-  const recent = useSelector((state) => state.post.recentPost);
-  console.log(recent);
+  // const recent = useSelector((state) => state.post.recentPost);
+  // console.log(recent);
   // 최신 페이지 , useEffect에서 최신 데이터를 가져온다. , useSelector로 store의 데이터를 가져옴
 
   return (
     <Stwrapper>
       <div className='main'>
         <div className='main-box'>
-          {recent?.map((item, index) => {
-            if (index === randomData.length - 1) {
-              return (
-                <MainPostCard
-                  key={item.id}
-                  item={item}
-                  ref={setLastIntersectingData}
-                />
-              );
-            } else {
-              return <MainPostCard key={item.id} item={item} />;
-            }
-          })}
+          <>
+            {randomData?.map((item, index) => {
+              if (index === randomData.length - 1) {
+                return (
+                  <MainPostCard
+                    key={item.id}
+                    item={item}
+                    ref={lastIntersectingData}
+                  />
+                );
+              } else {
+                return <MainPostCard key={item.id} item={item} />;
+              }
+            })}
+          </>
         </div>
       </div>
     </Stwrapper>
@@ -106,5 +101,11 @@ const Stwrapper = styled.div`
       margin: -1rem;
       flex-wrap: wrap;
     }
+  }
+
+  .Loading {
+    display: block;
+    margin: 0 auto;
+    font-weight: bold;
   }
 `;
