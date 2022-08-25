61b22cdeb3eb9d48ae01a3a04e216c3fe6768da8
KoreanCodingMachine <pon03078@naver.com>
2022-08-26 00:09:47 +0900

    무한스크롤 구현 성공 드디어!!!!!!!!!

--- src/components/main/MainRecentContainer.jsx
+++ src/components/main/MainRecentContainer.jsx
@@ -6,20 +6,18 @@ import axios from 'axios';
 
 const MainRecentContainer = () => {
   const [randomData, setRandomData] = useState([]);
-  const pages = useRef(0);
-
-  const addPages = () => {
-    pages.current++;
-  };
-
-  const [lastIntersectingData, setLastIntersectingData] = useState(null);
+  const [page, setPage] = useState(0);
+  const lastIntersectingData = useRef(null);
   //fetching 함수
   const getRandomData = async () => {
     console.log('fetching함수 호출됨');
     try {
       const { data } = await axios.get(
-        `http://3.38.48.108/api/post?Category=NEW&size=10&page=${pages.current}`
+        `http://3.38.48.108/api/post?Category=NEW&size=10&page=${page}`
       );
+      if (!data) {
+        return;
+      }
       setRandomData(randomData.concat(data));
       console.log(randomData);
     } catch (error) {
@@ -31,18 +29,19 @@ const MainRecentContainer = () => {
   const onIntersect = (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
+        console.log('!?!?!?');
         //뷰포트에 마지막 이미지가 들어오고, page값에 1을 더하여 새 fetch 요청을 보내게됨 (useEffect의 dependency배열에 page가 있음)
-        pages.current++;
-        // 현재 타겟을 unobserve한다.
-        observer.unobserve(entry.target);
+        setPage((page) => page + 1);
+        // 현재 타겟을 observe한다.
+        observer.observe(entry.target); // unobserve가 아님
       }
     });
   };
 
   useEffect(() => {
-    console.log('page?', pages.current);
+    console.log('page?', page);
     getRandomData();
-  }, [pages.current]);
+  }, [page]);
 
   useEffect(() => {
     //observer 인스턴스를 생성한 후 구독
@@ -50,39 +49,20 @@ const MainRecentContainer = () => {
     if (lastIntersectingData) {
       observer = new IntersectionObserver(onIntersect, { threshold: 0.5 });
       //observer 생성 시 observe할 target 요소는 불러온 이미지의 마지막아이템(배열의 마지막 아이템)으로 지정
-      observer.observe(lastIntersectingData);
+      observer.observe(lastIntersectingData.current);
     }
     return () => observer && observer.disconnect();
   }, [lastIntersectingData]);
-  // 최신(Recent) 페이지
-  // 최신 페이지
-
-  // useEffect(() => {
-  //   dispatch(getRecentData());
-  // }, []);
-
-  // const recent = useSelector((state) => state.post.recentPost);
-  // console.log(recent);
-  // 최신 페이지 , useEffect에서 최신 데이터를 가져온다. , useSelector로 store의 데이터를 가져옴
 
   return (
     <Stwrapper>
       <div className='main'>
         <div className='main-box'>
           <>
-            {randomData?.map((item, index) => {
-              if (index === randomData.length - 1) {
-                return (
-                  <MainPostCard
-                    key={item.id}
-                    item={item}
-                    ref={setLastIntersectingData}
-                  />
-                );
-              } else {
-                return <MainPostCard key={item.id} item={item} />;
-              }
+            {randomData.map((item, index) => {
+              return <MainPostCard key={item.id} item={item} />;
             })}
+            <div ref={lastIntersectingData}>.</div>
           </>
         </div>
       </div>
